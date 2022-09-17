c2d942b3753c39cb5f0370bf7454c25a7f0b3430
cahito <cahito@gmail.com>
2022-09-17 10:38:08 -0300

    Feat - Requisito vinte e tres cumprido.

--- src/routers/motorcyclesRouter.ts
+++ src/routers/motorcyclesRouter.ts
@@ -7,6 +7,6 @@ router.post(BIKES, (req, res) => bikesController.create(req, res));
 router.get(BIKES, (req, res) => bikesController.read(req, res));
 router.get(BIKES_ID, (req, res) => bikesController.readOne(req, res));
 router.put(BIKES_ID, (req, res) => bikesController.update(req, res));
-// router.delete(BIKES_ID, (req, res) => bikesController.delete(req, res));
+router.delete(BIKES_ID, (req, res) => bikesController.delete(req, res));
 
 export default router;
