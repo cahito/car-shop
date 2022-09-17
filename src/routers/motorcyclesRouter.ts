537c4d54dd7fc2d32d664054cd77a42779c76db2
cahito <cahito@gmail.com>
2022-09-17 10:35:51 -0300

    Feat - Requisito vinte e um cumprido.

--- src/routers/motorcyclesRouter.ts
+++ src/routers/motorcyclesRouter.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { bikesController, BIKES } from './main';
+import { bikesController, BIKES, BIKES_ID } from './main';
 
 const router = Router();
 
 router.post(BIKES, (req, res) => bikesController.create(req, res));
 router.get(BIKES, (req, res) => bikesController.read(req, res));
-// router.get(BIKES_ID, (req, res) => bikesController.readOne(req, res));
+router.get(BIKES_ID, (req, res) => bikesController.readOne(req, res));
 // router.put(BIKES_ID, (req, res) => bikesController.update(req, res));
 // router.delete(BIKES_ID, (req, res) => bikesController.delete(req, res));
 
